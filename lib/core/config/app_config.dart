f2d1592c406b3800fff0006aca42507fe5e4ae9f
PG-Adpops <adops@propertyguru.ae>
2026-03-26 13:24:37 +0400

    feat:” updates

--- lib/core/config/app_config.dart
+++ lib/core/config/app_config.dart
@@ -2,6 +2,6 @@ class AppConfig {
   //static const String baseUrl = 'http://127.0.0.1:3000';
   //static const String baseUrl = 'http://192.168.70.14:3000';
   // static const String baseUrl = 'http://localhost:3000';
- // static const String baseUrl = 'http://192.168.70.243:3000';
- static const String baseUrl = 'https://api.kmstry.net';
+  // static const String baseUrl = 'http://192.168.70.243:3000';
+  static const String baseUrl = 'https://api.kmstry.net';
 }
